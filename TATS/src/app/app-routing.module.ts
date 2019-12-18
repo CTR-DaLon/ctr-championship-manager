ebb6b85812596896822ee2648dc35b995c6744a0
Benjamin Vianey <vianey.benjamin@protonmail.com>
2019-12-18 20:59:31 +0000

    Init for master

--- TATS/src/app/app-routing.module.ts
+++ TATS/src/app/app-routing.module.ts
@@ -3,9 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   {
-    path: 'championships',
+    path: 'home',
     loadChildren: () => import('./pages/championships/championships.module').then( m => m.ChampionshipsPageModule)
   },
   {
